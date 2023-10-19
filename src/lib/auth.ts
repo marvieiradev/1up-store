207eaf5995a9f367bccbd80ae6b79d6385644247
Marcos Vieira <marcosvieira482@gmail.com>
2023-10-19 16:10:46 -0300

    feat: add auth to handler component

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,8 +7,8 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
 };
