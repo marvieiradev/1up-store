87ea00c84b66faf01d39ff4c30c49222498993bc
Marcos Vieira <marcosvieira482@gmail.com>
2023-10-20 12:44:32 -0300

    style: ensure footer is aways on the botton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-        <AuthProvider>
-          <Header />
-          {children}
-          <Footer />
-        </AuthProvider>
+        <div className="flex h-full flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex1">{children}</div>
+            <Footer />
+          </AuthProvider>
+        </div>
       </body>
     </html>
   );
