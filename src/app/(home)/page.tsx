bc3fab4805baa11c092dcbf1208c64c1c00c63df
Marcos Vieira <marcosvieira482@gmail.com>
2023-10-20 09:42:17 -0300

    feat: add Banner, Categories and CategoriesItem

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Categories from "./components/categories";
+
+export default function Home() {
+  const { data } = useSession();
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês"
+      />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  );
+}
