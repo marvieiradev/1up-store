2b7dda0700955989028d517db2c119e3cd86d3b9
Marcos Vieira <marcosvieira482@gmail.com>
2023-10-23 20:24:51 -0300

    feat: add CartItem component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -2,11 +2,13 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import CartProvider, { CartContext } from "@/providers/cart";
 import { useContext } from "react";
+import CartItem from "./cart-item";
+import { computeProductTotalPrice } from "@/helpers/products";
 
 const Cart = () => {
-    const {products} = useContext(CartContext)
+  const { products } = useContext(CartContext);
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.4rem] text-base uppercase"
         variant="outline"
@@ -15,7 +17,14 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      {products.map(product => <h1 key={product.id}>{product.name}</h1>)}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={computeProductTotalPrice(product as any) as any}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/helpers/products.ts
+++ src/helpers/products.ts
@@ -4,7 +4,9 @@ export interface ProductWithTotalPrice extends Product {
   totalPrice: number;
 }
 
-export const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
+export const computeProductTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
   if (product.discountPercentage === 0) {
     return {
       ...product,
