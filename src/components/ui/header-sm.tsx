66f2df4bcd7cec449bfdb2c17b07eefe97ad60eb
Marcos Vieira <marcosvieira482@gmail.com>
2023-10-31 22:03:44 -0300

    fix: window is not defined

--- src/components/ui/header-sm.tsx
+++ src/components/ui/header-sm.tsx
@@ -41,132 +41,134 @@ const HeaderSm = () => {
   };
 
   return (
-    <Card className="flex justify-between p-[1.875rem] py-[1rem] md-px-10 lg:px-20 lg:py-[0.5rem] items-center">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader className="text-left text-lg font-semibold">
-            Menu
-          </SheetHeader>
-          {status === "authenticated" && data?.user && (
-            <div className="flex flex-col">
-              <div className="flex items-center gap-2 py-4">
-                <Avatar>
-                  <AvatarFallback>
-                    {data.user.name?.[0].toUpperCase()}
-                  </AvatarFallback>
+    <div>
+      <Card className="flex justify-between p-[1.875rem] py-[1rem] md-px-10 lg:px-20 lg:py-[0.5rem] items-center">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader className="text-left text-lg font-semibold">
+              Menu
+            </SheetHeader>
+            {status === "authenticated" && data?.user && (
+              <div className="flex flex-col">
+                <div className="flex items-center gap-2 py-4">
+                  <Avatar>
+                    <AvatarFallback>
+                      {data.user.name?.[0].toUpperCase()}
+                    </AvatarFallback>
 
-                  {data.user.image && <AvatarImage src={data.user.image} />}
-                </Avatar>
+                    {data.user.image && <AvatarImage src={data.user.image} />}
+                  </Avatar>
 
-                <div className="flex flex-col">
-                  <p className="font-medium">{data.user.name}</p>
-                  <p className="text-sm opacity-75">Boas compras!</p>
+                  <div className="flex flex-col">
+                    <p className="font-medium">{data.user.name}</p>
+                    <p className="text-sm opacity-75">Boas compras!</p>
+                  </div>
                 </div>
+
+                <Separator />
               </div>
+            )}
 
-              <Separator />
+            <div className="mt-2 flex flex-col gap-2">
+              {status === "unauthenticated" && (
+                <Button
+                  onClick={handleLoginClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogInIcon size={16} />
+                  Fazer Login
+                </Button>
+              )}
+
+              {status === "authenticated" && (
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon size={16} />
+                  Fazer Logout
+                </Button>
+              )}
+              <SheetClose asChild>
+                <Link href="/">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <HomeIcon size={16} />
+                    Início
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/orders">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <ShoppingBasket size={16} />
+                    Meus Pedidos
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/deals">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <PercentIcon size={16} />
+                    Ofertas
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/catalog">
+                  <Button
+                    variant="outline"
+                    className="w-full justify-start gap-2"
+                  >
+                    <ListOrderedIcon size={16} />
+                    Catálogo
+                  </Button>
+                </Link>
+              </SheetClose>
             </div>
-          )}
+          </SheetContent>
+        </Sheet>
+        <Link href="/">
+          <h1 className="text-lg font-semibold">
+            <span className="font-bold text-primary">1UP</span> Store
+          </h1>
+        </Link>
 
-          <div className="mt-2 flex flex-col gap-2">
-            {status === "unauthenticated" && (
-              <Button
-                onClick={handleLoginClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogInIcon size={16} />
-                Fazer Login
-              </Button>
-            )}
-
-            {status === "authenticated" && (
-              <Button
-                onClick={handleLogoutClick}
-                variant="outline"
-                className="w-full justify-start gap-2"
-              >
-                <LogOutIcon size={16} />
-                Fazer Logout
-              </Button>
-            )}
-            <SheetClose asChild>
-              <Link href="/">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <HomeIcon size={16} />
-                  Início
-                </Button>
-              </Link>
-            </SheetClose>
-
-            <SheetClose asChild>
-              <Link href="/orders">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <ShoppingBasket size={16} />
-                  Meus Pedidos
-                </Button>
-              </Link>
-            </SheetClose>
-
-            <SheetClose asChild>
-              <Link href="/deals">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <PercentIcon size={16} />
-                  Ofertas
-                </Button>
-              </Link>
-            </SheetClose>
-
-            <SheetClose asChild>
-              <Link href="/catalog">
-                <Button
-                  variant="outline"
-                  className="w-full justify-start gap-2"
-                >
-                  <ListOrderedIcon size={16} />
-                  Catálogo
-                </Button>
-              </Link>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
-      <Link href="/">
-        <h1 className="text-lg font-semibold">
-          <span className="font-bold text-primary">1UP</span> Store
-        </h1>
-      </Link>
-
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline" className="relative">
-            <ShoppingCartIcon />
-            {products.length > 0 && (
-              <Badge className="absolute text-xs left-6 top-0 py-0 px-1">
-                <span>{products.length}</span>
-              </Badge>
-            )}
-          </Button>
-        </SheetTrigger>
-        <SheetContent className="w-[350px]">
-          <Cart />
-        </SheetContent>
-      </Sheet>
-    </Card>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="relative">
+              <ShoppingCartIcon />
+              {products.length > 0 && (
+                <Badge className="absolute text-xs left-6 top-0 py-0 px-1">
+                  <span>{products.length}</span>
+                </Badge>
+              )}
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[350px]">
+            <Cart />
+          </SheetContent>
+        </Sheet>
+      </Card>
+    </div>
   );
 };
 
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,23 +3,29 @@
 import HeaderLg from "./header-lg";
 import HeaderSm from "./header-sm";
 import { useRouter } from "next/navigation";
+import useWindowSize from "@rooks/use-window-size"
 
 const Header = () => {
   const router = useRouter();
-  const { innerWidth: width } = window;
+  const { innerWidth } = useWindowSize();
   const resizes = () => {
     router.refresh();
   }
-  window.addEventListener("resize", resizes);
+
+  if (typeof window !== "undefined") {
+    window.addEventListener("resize", resizes);
+  }
 
   return (
-    <div>
-      {width > 769 ? (
-        <HeaderLg />
-      ) : (
-        <HeaderSm />
-      )}
-    </div>
+    <>
+      <div suppressHydrationWarning={true}>
+        {innerWidth > 769 ? (
+          <HeaderLg />
+        ) : (
+          <HeaderSm />
+        )}
+      </div>
+    </>
   );
 };
 
