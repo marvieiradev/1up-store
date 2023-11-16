ad33544500c3025fd21d8dd24d732ce17ab50434
Marcos Vieira <marcosvieira482@gmail.com>
2023-11-16 18:31:11 -0300

    style: improvement in order page design

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -43,7 +43,7 @@ async function OrderPage() {
           Meus pedidos
         </Badge>
 
-        <div className="flex flex-col gap-5 mt-5 md:grid md:grid-cols-2 md:px-10 lg:px-20">
+        <div className="flex flex-col gap-5 mt-5 md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 md:px-10 lg:px-20">
           {orders.map((order) => (
             <OrderItem key={order.id} order={order} />
           ))}
